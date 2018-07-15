6991197aaf30f542641551d3e1fc3fe3a01f154f
ironmann250 <ironmann350@outlook.com>
2018-07-15 16:18:42 +0800

    added heroku db selector

--- tikeweb/settings.py
+++ tikeweb/settings.py
@@ -74,13 +74,18 @@ WSGI_APPLICATION = 'tikeweb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-DATABASES = {
-    'default': {
+
+if os.environ.get('PRODUCTION'):
+    import dj_database_url
+    db_from_env = dj_database_url.config(conn_max_age=500, require_ssl=True)
+    DATABASES['default'].update(db_from_env)
+else: 
+    DATABASES = {
+        'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
