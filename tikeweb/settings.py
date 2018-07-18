5cf458cdac99bf34b55c65ec0806c0c14aca31f3
ironmann250 <ironmann350@outlook.com>
2018-07-18 21:50:36 +0800

    save main event

--- tikeweb/settings.py
+++ tikeweb/settings.py
@@ -74,19 +74,21 @@ WSGI_APPLICATION = 'tikeweb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-    'ENGINE': 'django.db.backends.sqlite3',
-    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
-'''
 if os.environ.get('PRODUCTION') != None:
     import dj_database_url
     import dj_database_url
+    DATABASES ={}
     DATABASES['default'] = dj_database_url.config()
-'''
+
+else: 
+    DATABASES = {
+        'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
 
